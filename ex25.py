3751de69bd8765d126934a32ca54d3028c78e2a5
Justin Uy <justin.uy@codazen.com>
2015-04-09 23:09:19 -0700

    some more exercises I forgot to commit before starting blackjack game

---
+++ ex25.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#this lesson just introduces a few standard library functions
+# {string}.split(delimiter)
+# {list}.sorted()
+# {list}.pop(0) -- first element in list
+# {list}.pop(-1) -- last element in list
+
---
+++ ex27.py
@@ -0,0 +1,7 @@
+#This is just about learning logic
+
+# and / or for compound conditions
+# == != < > <= >= operators
+# not must be used for negations
+# True and False are bool values
+
